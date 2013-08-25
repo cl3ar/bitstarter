8d79e01f6b175a271ec5ae9b0a85e06127d3cdad
Ubuntu <ubuntu@ip-172-31-8-252.us-west-2.compute.internal>
2013-08-25 22:19:33 +0000

    made fix for extra arg to fs.readFileSync

--- web.js
+++ web.js
@@ -4,15 +4,8 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-   fs.readFileSync('index.html', function (err, data) {
-    if (err) {
-        console.log("file read fail");
-        throw err;
-    }
-    else{
-        response.send(data);
-    }
-   });
+   var data = fs.readFileSync('index.html');
+   response.send(data);
 });
 
 var port = process.env.PORT || 5000;
